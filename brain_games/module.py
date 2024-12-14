25a078f68c1f49802fbc2b5c2700f580c2441165
Rustem <rustemkago12345@gmail.com>
2024-12-14 15:27:17 +0300

    brain_games.py and module.py

--- brain_games/module.py
+++ brain_games/module.py
@@ -1,4 +1,8 @@
 from brain_games.cli import welcome_user
 
-def greet():
-    welcome_user
+def main():
+    welcome_user()
+
+
+if __name__ == "__main__":
+    main()
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -2,7 +2,3 @@ from brain_games.cli import welcome_user
 
 def main():
     welcome_user()
-
-
-if __name__ == "__main__":
-    main()

a7f90e18ede2264d9280ce8d22ca31a04838ceaa
Rustem <rustemkago12345@gmail.com>
2024-12-17 09:03:17 +0300

    dir games

---
+++ brain_games/games/even_games.py
@@ -0,0 +1,18 @@
+from random import randint
+from brain_games.constants import EVEN_INSTRUCTION
+from brain_games.engine import run_game
+
+def is_even(num):
+    return num % 2 == 0
+
+def get_random_num_and_answer():
+
+    random_num = randint(1, 100)
+    question = str(random_num)
+    correct_answer = 'yes' if is_even(random_num) else 'no'
+    
+    return question, correct_answer
+
+def run_even_game():
+    run_game(get_random_num_and_answer, EVEN_INSTRUCTION)
+

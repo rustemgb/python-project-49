9f91ec47c245c4b248340575cbb54155364fd883
Rustem <rustemkago12345@gmail.com>
2024-12-21 09:45:24 +0300

    add calc_games and brain_calc

---
+++ brain_games/games/calc_games.py
@@ -0,0 +1,26 @@
+from random import randint, choice
+from brain_games.constants import CALC_INSTRUCTION, CALC_ARITHMETIC
+from brain_games.engine import run_game
+
+def calc_answer(num_1, num_2, arithmetic):
+    if arithmetic == "+":
+        return num_1 + num_2
+    elif arithmetic == "-":
+        return num_1 - num_2
+    elif arithmetic == "*":
+        return num_1 * num_2
+
+def get_arithmetic_and_answer():
+
+    num_1 = randint(0, 30)
+    num_2 = randint(0, 30)
+
+    arithmetic = choice(CALC_ARITHMETIC)
+    question = f'{num_1} {arithmetic} {num_2}'
+    correct_answer = calc_answer(num_1, num_2, arithmetic)
+
+    return question, correct_answer
+
+def run_calc_game():
+    run_game(get_arithmetic_and_answer, CALC_INSTRUCTION)
+
---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,8 @@
+from brain_games.games.calc_games import run_calc_game
+
+def main():
+    run_calc_game()
+
+
+if __name__ == "__main__":
+    main()

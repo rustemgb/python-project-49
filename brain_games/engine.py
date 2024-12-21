210aa2b528dfcca07ee1b3c5e27e89c58c3767dc
Rustem <rustemkago12345@gmail.com>
2024-12-21 09:46:13 +0300

    fix engine.py

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -1,19 +1,19 @@
 import prompt
 
-def run_game(game, insructiom):
+def run_game(question_and_answer, insructiom):
     name = prompt.string('Welcome to the Brain Games!\nMay I have your name? ')
     print(f'Hello, {name}!\n{insructiom}')
 
     for _ in range(3):
-        question, correct_answer = game()
+        question, correct_answer = question_and_answer()
         print(f'Question: {question}')
         user_answer = prompt.string('Your answer: ')
 
-        if user_answer == correct_answer:
+        if user_answer == str(correct_answer):
             print('Correct!')
 
         else:
-            print(f"'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, {name}!")
+            print(f"{user_answer} is wrong answer ;(. Correct answer was {correct_answer}.\nLet's try again, {name}!")
         
             return
     

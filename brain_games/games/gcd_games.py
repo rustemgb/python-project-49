6823143ee8cba613a098867973a54c846de24828
Rustem <rustemkago12345@gmail.com>
2024-12-23 11:43:03 +0300

    fix gcd_games.py

--- brain_games/games/gcd_games.py
+++ brain_games/games/gcd_games.py
@@ -15,16 +15,16 @@ def calc_correct_answer(num_1, num_2):
             divider -= 1
 
 
-def get_quetion_and_answer():
+def get_numbers_and_answer():
 
     num_1 = randint(0, 100)
     num_2 = randint(0, 100)
 
-    question = f'{num_1} {num_2}'
+    numbers = f'{num_1} {num_2}'
     correct_answer = calc_correct_answer(num_1, num_2)
 
-    return question, correct_answer
+    return numbers, correct_answer
 
 
 def run_gcd_games():
-    run_game(get_quetion_and_answer, GCD_INSRUCTION)
+    run_game(get_numbers_and_answer, GCD_INSRUCTION)

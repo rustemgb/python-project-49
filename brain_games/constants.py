ec0f9dea046abafb13daea4a1a869bd5c323064c
Rustem <rustemkago12345@gmail.com>
2024-12-23 13:06:14 +0300

    fix constants.py

--- brain_games/constants.py
+++ brain_games/constants.py
@@ -1,4 +1,5 @@
 EVEN_INSTRUCTION = 'Answer "yes" if the number is even, otherwise answer "no".'
 CALC_INSTRUCTION = 'What is the result of the expression?'
 CALC_ARITHMETIC = "+", "-", "*"
-GCD_INSRUCTION = 'Find the greatest common divisor of given numbers.'
+GCD_INSTRUCTION = 'Find the greatest common divisor of given numbers.'
+PROGRESSION_INSTRUCTION = 'What number is missing in the progression?'

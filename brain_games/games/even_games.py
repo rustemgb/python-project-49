f1518efcf4dba30518a8a8ef715049a90a45a1e6
Rustem <rustemkago12345@gmail.com>
2024-12-26 08:48:46 +0300

    fix even_games.py

--- brain_games/games/even_games.py
+++ brain_games/games/even_games.py
@@ -1,9 +1,12 @@
 from random import randint
+
 from brain_games.constants import EVEN_INSTRUCTION
 from brain_games.engine import run_game
 
+
 def is_even(num):
     return num % 2 == 0
+
 
 def get_random_num_and_answer():
 
@@ -12,6 +15,7 @@ def get_random_num_and_answer():
     correct_answer = 'yes' if is_even(random_num) else 'no'
     
     return question, correct_answer
+
 
 def run_even_game():
     run_game(get_random_num_and_answer, EVEN_INSTRUCTION)
